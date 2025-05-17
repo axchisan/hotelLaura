2c74daa6284885b078d61d6c34043050b8122459
axchisan <leftyrancuentabot@gmail.com>
2025-05-16 23:18:14 -0500

    volvi a dejar la base de datos en localhost

--- config/config.php
+++ config/config.php
@@ -1,10 +1,10 @@
 <?php
-define('DB_HOST', '127.0.0.1');
+define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'hotelau');
 
-define('BASE_URL', 'http://localhost/hotel');
+define('BASE_URL', 'http://localhost/hotelau');
 define('APP_ROOT', dirname(dirname(__FILE__)));
 
 define('SESSION_TIMEOUT', 1800); 
